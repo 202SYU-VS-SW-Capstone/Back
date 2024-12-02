b1bc5eb0dda7fa9ca3e47da10b72a7b6c4b8c644
blackdemon129 <blackdemon129@gmail.com>
2024-12-03 07:07:51 +0900

    Comment API 구동을 위한 엔티티 및 기타 API 수정
    Comment
    API의 정상 구동 모두 확인

--- Backend-main/recipedemo/src/main/java/com/ohgiraffers/recipeapp/entity/Comment.java
+++ Backend-main/recipedemo/src/main/java/com/ohgiraffers/recipeapp/entity/Comment.java
@@ -3,6 +3,8 @@ package com.ohgiraffers.recipeapp.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "comments")
 @Data
@@ -13,6 +15,7 @@ public class Comment {  // 댓글 테이블
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
     private Long id;
 
     @ManyToOne
@@ -25,4 +28,13 @@ public class Comment {  // 댓글 테이블
 
     @Column(nullable = false)
     private String content; // 댓글 내용
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
 }
